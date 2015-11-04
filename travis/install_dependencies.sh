3c1f431c3aaaaedd410c2cc480356bed52d3ed66
Philippe RG <phirg@users.noreply.github.com>
2015-11-04 22:43:30 +0100

    Update install_dependencies.sh

--- travis/install_dependencies.sh
+++ travis/install_dependencies.sh
@@ -15,6 +15,7 @@ then
     sudo apt-get remove cmake -q
     sudo apt-get autoremove -q
     CMAKE_VERSION="3.0.0"
+    wget -V
     wget "https://www.cmake.org/files/v3.0/cmake-${CMAKE_VERSION}.tar.gz"
     tar xf "cmake-${CMAKE_VERSION}.tar.gz"
     cd "cmake-${CMAKE_VERSION}"
