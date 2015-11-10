9c52d147a50d11e17f814c5019257a0ac129d409
Philippe RG <phirg@users.noreply.github.com>
2015-11-10 11:10:28 +0100

    Update install_dependencies.win.sh

--- appveyor/install_dependencies.win.sh
+++ appveyor/install_dependencies.win.sh
@@ -16,18 +16,29 @@ git checkout $POCO_TAG_NAME
 # build using cmake
 echo "building poco with cmake" # which components?
 cd ..; mkdir poco-build; cd poco-build
+POCO_INSTALL_DEST=$PROGRAMFILES/Poco
+echo "preparing install destination to $POCO_INSTALL_DEST"
 echo "CMake generator is: $GENERATOR"
 cmake ../poco -DENABLE_XML=ON -DENABLE_JSON=ON -DENABLE_MONGODB=OFF -DENABLE_PDF=OFF \
     -DENABLE_UTIL=ON -DENABLE_NET=OFF -DENABLE_NETSSL=OFF -DENABLE_NETSSL_WIN=OFF -DENABLE_CRYPTO=OFF \
     -DENABLE_DATA=OFF -DENABLE_DATA_SQLITE=OFF -DENABLE_DATA_MYSQL=OFF -DENABLE_DATA_ODBC=OFF \
     -DENABLE_SEVENZIP=OFF -DENABLE_ZIP=OFF -DENABLE_APACHECONNECTOR=OFF -DENABLE_CPPPARSER=OFF \
-    -DENABLE_POCODOC=OFF -DENABLE_PAGECOMPILER=OFF -DENABLE_PAGECOMPILER_FILE2PAGE=OFF -G"$GENERATOR" 
+    -DENABLE_POCODOC=OFF -DENABLE_PAGECOMPILER=OFF -DENABLE_PAGECOMPILER_FILE2PAGE=OFF \
+    -DCMAKE_INSTALL_PREFIX="$POCO_INSTALL_DEST" -G"$GENERATOR" 
 cmake --build . --config "RelWithDebInfo"
 echo "Poco build done. "
 cmake --build . --target install --config "RelWithDebInfo"
 echo "Poco install done. "
 
+PATH="$POCO_INSTALL_PREFIX/bin;$PATH"
 echo "path: $PATH"
+
+CMAKE_DIR=$(whereis cmake)
+echo "cmake is at: $CMAKE_DIR"
+ls "$CMAKE_DIR"
+echo "copying poco cmake files to the right place: $CMAKE_DIR/modules"
+cp "$POCO_INSTALL_DEST/lib/cmake/*.*" "$CMAKE_DIR/modules"
+ls "$CMAKE_DIR/modules"
 
 cd $CURRENT_DIR
 pwd
