dbdd1388d4fea5f1ac5c62b63dc819e4d07d39b5
Philippe RG <phirg@users.noreply.github.com>
2015-11-10 22:06:46 +0100

    Update convenience.cmake

--- appveyor/convenience.cmake
+++ appveyor/convenience.cmake
@@ -3,7 +3,5 @@
 set ( POCO_INSTALL_DEST CACHE PATH "Poco installation directory" )
 
 # mv ConfigPoco.cmake etc to the right place
-file ( GLOB_RECURSE 
-
 file ( COPY "${POCO_INSTALL_DEST}/lib/cmake/Poco/"
        DESTINATION "${CMAKE_ROOT}/Modules" ) 
