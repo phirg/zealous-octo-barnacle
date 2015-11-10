62791a9e08b133e4950d7c374db655719928bf29
phirg <phirg@users.noreply.github.com>
2015-11-10 23:35:29 +0100

    install script modification: translating to .bat

--- appveyor/install_dependencies.bat
+++ appveyor/install_dependencies.bat
@@ -1,39 +1,34 @@
-#!/bin/sh
+set CURRENT_DIR=%CD%
+echo "current dir is %CURRENT_DIR%"
 
-CURRENT_DIR=$(pwd)
-echo "current dir is $CURRENT_DIR"
+set POCO_INSTALL_PREFIX="%PROGRAMFILES%/Poco"
 
-POCO_INSTALL_PREFIX="/c/Program Files/Poco"
-
-# get last poco via github
+REM get last poco via github
 echo "getting the last poco release from github"
 mkdir dependencies
 cd dependencies
 git clone https://github.com/pocoproject/poco.git
 cd poco
-POCO_TAG_NAME=$(git describe --tags)
-echo "checking out poco at tag: $POCO_TAG_NAME"
-git checkout $POCO_TAG_NAME
 
-# build using cmake
-echo "building poco with cmake" # which components?
+REM now using msys to make things easier
+sh -c "POCO_TAG_NAME=$(git describe --tags) ; git checkout $POCO_TAG_NAME"
+
+REM build using cmake
+echo "building poco with cmake" 
 cd ..; mkdir poco-build; cd poco-build
 
-echo "preparing install destination to $POCO_INSTALL_PREFIX"
-echo "CMake generator is: $GENERATOR"
+echo "preparing install destination to %POCO_INSTALL_PREFIX%"
+echo "CMake generator is: %GENERATOR%"
 cmake ../poco -DENABLE_XML=ON -DENABLE_JSON=ON -DENABLE_MONGODB=OFF -DENABLE_PDF=OFF \
     -DENABLE_UTIL=ON -DENABLE_NET=OFF -DENABLE_NETSSL=OFF -DENABLE_NETSSL_WIN=OFF -DENABLE_CRYPTO=OFF \
     -DENABLE_DATA=OFF -DENABLE_DATA_SQLITE=OFF -DENABLE_DATA_MYSQL=OFF -DENABLE_DATA_ODBC=OFF \
     -DENABLE_SEVENZIP=OFF -DENABLE_ZIP=OFF -DENABLE_APACHECONNECTOR=OFF -DENABLE_CPPPARSER=OFF \
     -DENABLE_POCODOC=OFF -DENABLE_PAGECOMPILER=OFF -DENABLE_PAGECOMPILER_FILE2PAGE=OFF \
-    -DCMAKE_INSTALL_PREFIX="$POCO_INSTALL_PREFIX" -G"$GENERATOR" 
+    -DCMAKE_INSTALL_PREFIX="%POCO_INSTALL_PREFIX%" -G"%GENERATOR%" 
 cmake --build . --config "RelWithDebInfo"
 echo "Poco build done. "
 cmake --build . --target install --config "RelWithDebInfo"
 echo "Poco install done. "
 
-export PATH="$POCO_INSTALL_PREFIX/bin:$PATH"
-echo "new path is: $PATH"
-
-cd $CURRENT_DIR
-pwd
+set PATH=%PATH%;"%POCO_INSTALL_PREFIX%\bin"
+cd %CURRENT_DIR%
