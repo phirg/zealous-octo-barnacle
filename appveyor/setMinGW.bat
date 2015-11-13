8d93762d767394f593b2f7c481bb1e8baa94e092
Philippe RG <phirg@users.noreply.github.com>
2015-11-13 14:12:22 +0100

    Delete setMinGW.bat

--- appveyor/setMinGW.bat
+++
@@ -1,8 +0,0 @@
-if "%GENERATOR%"=="MinGW Makefiles" (
-  set SH_COMMAND=gulash
-  move "%PROGRAMFILES%\Git\usr\bin\sh.exe" "%PROGRAMFILES%\Git\usr\bin\%SH_COMMAND%.exe"
-  set PATH=%PATH%;%ProgramFiles(x86)%\Windows Kits\10\bin\x86
-  echo %PATH%
-) else (
-  set SH_COMMAND=sh 
-)
